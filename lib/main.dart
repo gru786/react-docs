b1d03ca561b7e0782f22116fb4ba9046dff6f2d1
Gargi Pal <gargipal@Gargis-Air.lan>
2024-06-23 23:27:10 +0530

    internet connection check added

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor:Colors.blueGrey.shade900),
--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,10 +11,16 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late WebViewController _controller;
+  bool _hasInternet = true;
 
   @override
   void initState() {
     super.initState();
+    _initializeWebView();
+    _checkInternetConnection();
+  }
+
+  void _initializeWebView() {
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setBackgroundColor(const Color(0x00000000))
@@ -38,10 +45,6 @@ Page resource error:
             ''');
           },
           onNavigationRequest: (NavigationRequest request) {
-            // if (request.url.startsWith('https://www.youtube.com/')) {
-            //   debugPrint('blocking navigation to ${request.url}');
-            //   return NavigationDecision.prevent;
-            // }
             debugPrint('allowing navigation to ${request.url}');
             return NavigationDecision.navigate;
           },
@@ -65,42 +68,76 @@ Page resource error:
       ..loadRequest(Uri.parse('https://react.dev/'));
   }
 
+  void _checkInternetConnection() async {
+    final listener =
+        InternetConnection().onStatusChange.listen((InternetStatus status) {
+      switch (status) {
+        case InternetStatus.connected:
+          // The internet is now connected
+          setState(() {
+            _hasInternet = true;
+          });
+          break;
+        case InternetStatus.disconnected:
+          // The internet is now disconnected
+          setState(() {
+            _hasInternet = false;
+          });
+          break;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xff23272f),
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: WebViewWidget(
-                controller: _controller,
-              ),
-            ),
-            Container(
-              color: const Color(0xff23272f),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        child: _hasInternet
+            ? Column(
                 children: [
-                  IconButton(
-                    icon:  Icon(Icons.arrow_back, color: Colors.grey[100],),
-                    onPressed: () async {
-                      if (await _controller.canGoBack()) {
-                        await _controller.goBack();
-                      }
-                    },
+                  Expanded(
+                    child: WebViewWidget(
+                      controller: _controller,
+                    ),
                   ),
-                  IconButton(
-                    icon:  Icon(Icons.home, color: Colors.grey[100],),
-                    onPressed: () {
-                      _controller.loadRequest(Uri.parse('https://react.dev/'));
-                    },
+                  Container(
+                    color: const Color(0xff23272f),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        IconButton(
+                          icon: Icon(
+                            Icons.arrow_back,
+                            color: Colors.grey[100],
+                          ),
+                          onPressed: () async {
+                            if (await _controller.canGoBack()) {
+                              await _controller.goBack();
+                            }
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(
+                            Icons.home,
+                            color: Colors.grey[100],
+                          ),
+                          onPressed: () {
+                            _controller
+                                .loadRequest(Uri.parse('https://react.dev/'));
+                          },
+                        ),
+                      ],
+                    ),
                   ),
                 ],
+              )
+            : Center(
+                child: Text(
+                  'No internet connection',
+                  style: TextStyle(color: Colors.grey[100]),
+                ),
               ),
-            ),
-          ],
-        ),
       ),
     );
   }
